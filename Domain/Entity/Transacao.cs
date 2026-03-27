33ff49321b6f481f3f3e6d4656c3e569aa7f97d8
Luiz Bortolanza <luizbortolanza0@gmail.com>
2026-03-27 17:59:57 -0300

    Add static web assets and update project configuration for Finnance
    - Created staticwebassets.build.json to manage static assets for the project.
    - Added msbuild.build.Finnance.props and msbuild.buildMultiTargeting.Finnance.props for MSBuild integration.
    - Introduced msbuild.buildTransitive.Finnance.proAdicionados imports transitivos de propriedades.
    Gerado o arquivo project.assets.json para rastrear dependências e assets dos pacotes.
    Criados os arquivos nuget.g.props e nuget.g.targets para gerenciamento de pacotes NuGet.
    Atualizado o project.nuget.cache para refletir o estado atual das dependências.

---
+++ Domain/Entity/Transacao.cs
@@ -0,0 +1,47 @@
+namespace Finnace.Domain.Entity;
+
+class Transacao
+{
+  public int Id { get; private set; }
+  public Guid UserId { get; private set;}
+  public double Value { get ; private set;}
+  public string Type { get; private set; } 
+  public string Category { get; private set; }
+  public string Name { get; private set;}
+
+  private Transacao(double _value, string _type, string _category, string _name, Guid _userId)
+  {
+    UserId = _userId;
+    Value =_value;
+    Type = _type;
+    Category = _category;
+    Name = _name;
+  }
+
+  public static Transacao Create(double _value, string _type, string _category, string _name, Guid _userId)
+  {
+    if(_value<= 0)
+    {
+      throw new ArgumentException("O valor deve ser maior que zero!");
+    }
+    if (string.IsNullOrWhiteSpace(_category))
+    {
+      throw new ArgumentException("Categoria nao pode ser um campo vazio!");
+    }
+    if (string.IsNullOrWhiteSpace(_type))
+    {
+      throw new ArgumentException("Tipo nao pode ser um campo vazio!");
+    }
+    if (string.IsNullOrWhiteSpace(_name))
+    {
+      throw new ArgumentException("Nome da transacao nao pode ser um campo vazio!");
+    }
+
+    if(_userId == Guid.Empty)
+    {
+      throw new ArgumentException("Id do usuario invalido!");
+    }
+
+    return new Transacao(_value,_type,_category,_name,_userId);
+  }
+}
---
+++ Domain/Entity/User.cs
@@ -0,0 +1,29 @@
+namespace Finnace.Domain.Entity;
+
+public class User
+{
+  public Guid Id { get; private set; }
+  public string UserName { get; private set; } 
+  private string PasswordHash {get;set;}
+
+  private User(string _userName, string _passwordHash)
+  {
+    Id = Guid.NewGuid();
+    UserName = _userName;
+    PasswordHash = _passwordHash;
+  }
+  public static User Create(string _userName, string _passwordHash)
+  {
+    if(string.IsNullOrWhiteSpace(_userName))
+    {
+      throw new ArgumentException("Campo do usuario vazio!");
+    }
+
+    if(string.IsNullOrWhiteSpace(_passwordHash))
+    {
+      throw new ArgumentException("Campo de senha vazio!");
+    }
+
+    return new User(_userName,_passwordHash);
+  }
+}
